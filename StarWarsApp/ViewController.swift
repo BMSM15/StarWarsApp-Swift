4f6018e27a497546b04b987fc773756a5308dce5
Bruno Martins <brunomiguelmartins1522@gmail.com>
2024-07-17 12:26:13 +0100

    Initial Commit

---
+++ StarWarsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarWarsApp
+//
+//  Created by Bruno Martins on 17/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
